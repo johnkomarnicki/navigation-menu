de778d95cfd14d3de1984703de5cc662d07c5a80
JohnRay1997 <johnkomarnickicontact@gmail.com>
2021-05-13 09:47:49 -0400

    Update Starter Files

--- src/App.vue
+++ src/App.vue
@@ -10,4 +10,23 @@ export default {
 };
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+  font-family: "Raleway", sans-serif;
+  font-weight: 400;
+}
+
+.app {
+  min-height: 100vh;
+  position: relative;
+  background-color: #f1f1f1;
+}
+.container {
+  padding: 0 20px;
+  max-width: 1140px;
+  margin: 0 auto;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,17 @@
 <template>
-  <div class="home"></div>
+  <div class="home">
+    <section class="hero">
+      <div class="hero-text container">
+        <h4>Vue Responsive Nav Menu</h4>
+        <hr />
+        <h2>
+          With Vue <br />
+          Animations
+        </h2>
+        <hr />
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>
@@ -10,3 +22,51 @@ export default {
   components: {},
 };
 </script>
+
+<style lang="scss" scoped>
+.hero {
+  background-image: url("../assets/hero-bg.png");
+  background-attachment: fixed;
+  position: relative;
+  height: 100vh;
+
+  img {
+    object-fit: cover;
+    height: 100%;
+    width: 100%;
+  }
+
+  .hero-text {
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    color: #fff;
+
+    h4 {
+      text-transform: uppercase;
+      font-size: 22px;
+      padding-bottom: 4px;
+    }
+
+    h2 {
+      font-size: 50px;
+      @media (min-width: 550px) {
+        font-size: 80px;
+      }
+    }
+
+    hr:nth-child(2) {
+      max-width: 365px;
+      margin-bottom: 16px;
+    }
+    hr:nth-child(4) {
+      height: 6px;
+      background-color: #fff;
+      border: none;
+      max-width: 85px;
+      margin-top: 16px;
+    }
+  }
+}
+</style>
